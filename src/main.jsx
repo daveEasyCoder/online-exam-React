72710c2e3aa35285c01f50742b1cec7c5b50950d
daveEasyCoder <dawittechguru@gmail.com>
2025-04-11 09:02:32 +0300

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import GlobalContextProvider from "../components/GlobalContext/Context.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/online-exam-React">
       <GlobalContextProvider>
          <App />
       </GlobalContextProvider>
